2ac2ebb39dc8fd59f86ba23fffdc606128f791a4
alessiogandelli <alessiogandelli99@gmail.com>
2023-01-12 02:34:43 +0100

    remove queue

--- src/bot.ts
+++ src/bot.ts
@@ -23,11 +23,7 @@ bot.start((ctx) => ctx.reply('Hi! sono vivo!'));
 
 
 // ---------- Notifications ----------
-const toSend: {
-    name: string;
-    title: string;
-    link: string;
-}[] = []
+
 
 // every new news 
 feeder.on('new-item', (item: any) => {
@@ -37,11 +33,9 @@ feeder.on('new-item', (item: any) => {
             let firstname = item.title.split(' ')[2]
             let surname = item.title.split(' ')[3]
 
-            toSend.push({
-                name: firstname + ' ' + surname,
-                title: item.title,
-                link: item.link,
-            });
+            sendMorto(item)
+
+
             
 
 
@@ -50,26 +44,26 @@ feeder.on('new-item', (item: any) => {
 });
 
 
-// every 5 minutes
-setInterval(async () => {
-    const next = toSend.pop();
-    if (next) {
-        const res = (await og({ url: next.link })).result as any;
-        const description = res?.ogDescription ?? '';
-        const imgUrl = res?.ogImage?.url ?? null;
-        const siteName = res?.ogSiteName ?? res?.alAndroidAppName ?? '';
-        const section = res?.articleSection ?? '';
+async function sendMorto(item: any){
+    const res = (await og({ url: item.link })).result as any;
+    const description = res?.ogDescription ?? '';
+    const imgUrl = res?.ogImage?.url ?? null;
+    const siteName = res?.ogSiteName ?? res?.alAndroidAppName ?? '';
+    const section = res?.articleSection ?? '';
 
-        try {
-            bot.telegram.sendPhoto(channelid, imgUrl, {
-                caption: `<a href="${next.link}">${next.title}</a>\n\n${description}\n\n<b>${siteName}</b> ${section ? '#' + section.replace(' ', '') : ''}`,
-                parse_mode: 'HTML',
-            });
-        } catch (e) {
-            console.log(`errore nell'inviare il `);
+    try {
+        bot.telegram.sendPhoto(channelid, imgUrl, {
+            caption: `<a href="${item.link}">${item.title}</a>\n\n${description}\n\n<b>${siteName}</b> ${section ? '#' + section.replace(' ', '') : ''}`,
+            parse_mode: 'HTML',
+        });
+    } catch (e) {
+        console.log(`errore nell'inviare il `);
         }
-    }
-}, 500);
+}
+
+
+
+
 
 feeder.on('error', () => {});
 
